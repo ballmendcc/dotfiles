7c1c7a71399e9726687a44bcfc07999897b348b8
Benedikt Allmendinger <ballmend76@gmail.com>
2024-08-23 17:55:56 +0200

    chore: Refactor switch-window.sh script to remove unnecessary argument check

--- roles/gsettings/files/switch-window.sh
+++ roles/gsettings/files/switch-window.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is passed
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
     echo "Usage: $0 <workspace_number>"
     exit 1
 fi
@@ -10,7 +10,7 @@ fi
 active_window_id=$(xdotool getactivewindow)
 
 # Directly use the workspace number argument as it is 1-indexed
-workspace_number=$1
+workspace_number=$(echo "$1" | tr -d '[:space:]')
 
 # Move the active window to the specified workspace
 wmctrl -i -r "$active_window_id" -t $workspace_number
