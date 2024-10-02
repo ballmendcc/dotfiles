b92d26ef2fde1d4b97c55a101f659a07e6efa079
ballmendcc <ballmend33@gmail.com>
2024-10-02 08:57:21 +0200

    Refactor zsh docker aliases and add goconda alias

--- roles/bash/files/zsh/docker_aliases.sh
+++ roles/bash/files/zsh/docker_aliases.sh
@@ -2,3 +2,5 @@
 
 # alias dprune='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
 # alias dsysprune='docker system prune -af'
+
+alias goconda=eval "$(/home/ballmend/anaconda3/bin/conda shell.zsh hook)"
