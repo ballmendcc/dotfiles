906a57d98312d555ec2737d4f3b5ef96b4c98e4f
Benedikt Allmendinger <127944836+ballmendcc@users.noreply.github.com>
2024-10-02 14:30:32 +0200

    Update docker_aliases.sh

--- roles/bash/files/zsh/docker_aliases.sh
+++ roles/bash/files/zsh/docker_aliases.sh
@@ -3,4 +3,4 @@
 # alias dprune='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
 # alias dsysprune='docker system prune -af'
 
-alias goconda=eval "$(/home/ballmend/anaconda3/bin/conda shell.zsh hook)"
+alias goconda='eval "$(/home/ballmend/anaconda3/bin/conda shell.zsh hook)"'
